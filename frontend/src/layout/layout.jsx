602f441a8e69159ef35671699170d9e922160d28
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-09-29 11:21:20 +0530

    add layout file

---
+++ frontend/src/layout/layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import Routers from '../routes/Routers';
+
+
+const layout = () => {
+  return (
+   <>
+          <Header />
+          <main>
+            <Routers />
+          </main>
+          <Footer />
+   </>
+  )
+}
+
+export default layout
