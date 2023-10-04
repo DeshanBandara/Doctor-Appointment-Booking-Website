91cb7cf850208b7e365153cb7d4fdd5f9dfa2e40
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-04 12:57:58 +0530

    modified

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { useState } from 'react'
+import Home from '../pages/Home'
+import Contact from '../pages/Contact'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Services from '../pages/Services'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorDetails from '../pages/Doctors/DoctorDetails';
+
+import { Routes, Route } from 'react-router-dom'
+
 
 const Routers = () => {
-  return (
-    <div>Routers</div>
-  )
+    return <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:id' element={<DoctorDetails />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/registr' element={<Signup />} />
+        <Route path='/services' element={<Services />} />
+    </Routes>
 }
 
 export default Routers
