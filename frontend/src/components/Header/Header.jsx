e7fadc0092cfba7d3bf84e28935fc7804b2518af
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-04 12:57:48 +0530

    modified

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import { useState } from 'react'
+import {useEffect,useRef} from 'react'
+import logo from '../../assets/images/logo.png'
+import {NavLink,Link} from 'react-router-dom'
+
+const navLinks = [
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+    path: '/doctors',
+    display: 'Doctors'
+  },
+  {
+    path: '/service',
+    display: 'Services'
+  },
+  {
+    path: '/contact',
+    display: 'Contact'
+  },
+]
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return <header className='header flex items-center'>
+    <div className='container'>
+      <div className='flex items-center justify-between'>
+        {/*======================= logo ========================*/}
+        <div>
+          <img src={logo} alt="" />
+        </div>
+
+        {/*======================= Menu ========================*/}
+        <div className='navigation'>
+          <ul className='menu flex items-center gap-[2.7rem]'>
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink
+                  to={link.path}
+                  className={navClass =>
+                    navClass.isActive
+                    ? 'text-primaryColor text-[16px] leading-7 font-[600]'
+                    : 'text-textColor text-[16px] leading-7 font-[500]'
+                  }
+                >
+                    {link.display}
+                </NavLink>
+              </li>
+            ))
+            }
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </header>
 }
 
 export default Header
