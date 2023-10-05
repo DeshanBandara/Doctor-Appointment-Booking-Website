42cb9bf6a0fbcd60b821ede5dec842677590bd02
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-05 18:21:48 +0530

    added side avigation

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -45,6 +45,8 @@ const Header = () => {
 
   })
 
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
   return <header className='header flex items-center' ref={headerRef}>
     <div className='container'>
       <div className='flex items-center justify-between'>
@@ -54,7 +56,7 @@ const Header = () => {
         </div>
 
         {/*======================= Menu ========================*/}
-        <div className='navigation'>
+        <div className='navigation' ref={menuRef} onClick={toggleMenu}>
           <ul className='menu flex items-center gap-[2.7rem]'>
             {navLinks.map((link, index) => (
               <li key={index}>
@@ -90,7 +92,7 @@ const Header = () => {
                 justify-center rounded-[50px]'>Login</button>
             </Link>
 
-            <span className='md:hidden'>
+            <span className='md:hidden' onClick={toggleMenu}>
               <BiMenu className='w-6 h-6 cursor-pointer'/>
             </span>
         </div>
