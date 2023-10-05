dd19763ae56325bd32320d77b82ae2b357708c53
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-05 18:05:07 +0530

    updated

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import {useEffect,useRef} from 'react'
 import logo from '../../assets/images/logo.png'
 import userImg from '../../assets/images/avatar-icon.png'
 import {NavLink,Link} from 'react-router-dom'
+import {BiMenu} from 'react-icons/bi'
 
 const navLinks = [
   {
@@ -23,7 +24,28 @@ const navLinks = [
 ]
 
 const Header = () => {
-  return <header className='header flex items-center'>
+
+    const headerRef = useRef(null)
+    const menuRef = useRef(null)
+
+    const handleStickyHeader = () => {
+      window.addEventListener('scroll', ()=> {
+        if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+          headerRef.current.classList.add('sticky__header')
+        }else{
+          headerRef.current.classList.remove('sticky__header')
+        }
+      })
+    }
+
+  useEffect(()=>{
+    handleStickyHeader()
+
+    return ()=> window.removeEventListener('scroll', handleStickyHeader)
+
+  })
+
+  return <header className='header flex items-center' ref={headerRef}>
     <div className='container'>
       <div className='flex items-center justify-between'>
         {/*======================= logo ========================*/}
@@ -54,7 +76,7 @@ const Header = () => {
 
         <div className='flex items-center gap-4'>
 
-            <div>
+            <div className='hidden'>
               <Link to='/'>
                 <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
                   <img src={userImg} className='w-full rounded-full' alt="" />
