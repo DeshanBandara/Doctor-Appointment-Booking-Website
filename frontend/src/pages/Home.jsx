7bead2bc637ad35cb6906be6772fb98875b53c34
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-14 08:57:24 +0530

    added feature section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,9 +6,13 @@ import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
 import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
+import featureImg from '../assets/images/feature-img.png'
+import videoIcon from '../assets/images/video-icon.png'
+import avatarIcon from '../assets/images/avatar-icon.png'
 import { Link } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
 import About from '../components/About/About'
+import ServiceList from '../components/Services/ServiceList'
  
 const Home = () => {
   return <>
@@ -180,10 +184,73 @@ const Home = () => {
               Word-class care for everyone. Our health system offers unmatched, expert health care.
             </p>
           </div>
+
+        <ServiceList />
+
         </div>
       </section>
 
     {/* ============== Service section end ============== */}
+
+    {/* ============== Feature section ============== */}
+    <br /><br /><br />
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+          {/* ============== Feature content ============== */}
+          <div className="xl:w-[670px]">
+            <h2 className='heading  m-6'>
+              Get virtual treatment <br /> Anytime
+            </h2>
+            <ul className='pl-4'>
+              <li className='text__para mb-3'>1. Schedule the appoinment directly.</li>
+              <li className='text__para mb-3'>2. Search your physician here, and contact their office.</li>
+              <li className='text__para'>3. View your physician who are accepting new patients, use the online
+              scheduling tool to select an appoinment time.</li>
+            </ul>
+            <Link to="/">
+              <button className='btn ml-3'>Learn More</button>
+            </Link>
+          </div>
+
+            {/* ============== Feature img ============== */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} className='w-3/4' alt="" />
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px]
+              md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-black font-[600]'>
+                      Tue, 24
+                    </p>
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]'>
+                      10:00AM
+                    </p>
+                  </div>
+                  <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellow-300
+                  rounded py-1 px-[6px] lg:py-3 lg:px-[9px]'>
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+
+                <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-8px
+                leading-[8px] lg:text-[12px] lg:leading-4 text-[#246BCE] font-[500] mt-2 lg:mt-4 rounded-full'>
+                  Consultation
+                </div>
+                <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <img src={avatarIcon} alt="" />
+                  <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-textColor'>Wayne Collins</h4>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    {/* ============== Feature section end ============== */}
+
+    {/* ============== Our great doctors ============== */}
+    {/* ============== Our great doctors end ============== */}
 
   </>
 }
