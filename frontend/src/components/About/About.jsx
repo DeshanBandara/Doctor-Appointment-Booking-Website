f6880548cb5ef2542ef83f955bc7f70078473d64
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-14 06:38:51 +0530

    added images

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import aboutImg from '../../assets/images/about.png'
+import aboutCardImg from '../../assets/images/about-card.png'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section>
+        <div className='container'>
+            <div className='flex justify-between gap-[50px] lg:gap-[130] xl:gap-0 flex-col lg:flex-raw' >
+                {/* ================= About img ================== */}
+                <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
+                    <img src={aboutImg} alt="" />
+                    <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-7%] lg:right-[-22%]">
+                        <img src={aboutCardImg} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
