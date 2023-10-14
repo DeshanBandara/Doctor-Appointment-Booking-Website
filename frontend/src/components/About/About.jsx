c1f3174edd476177498c86fcd957437fcd1d62ac
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-14 07:08:09 +0530

    added image and conetent in about section

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
 import aboutImg from '../../assets/images/about.png'
 import aboutCardImg from '../../assets/images/about-card.png'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <section>
-        <div className='container'>
-            <div className='flex justify-between gap-[50px] lg:gap-[130] xl:gap-0 flex-col lg:flex-raw' >
-                {/* ================= About img ================== */}
-                <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
-                    <img src={aboutImg} alt="" />
-                    <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-7%] lg:right-[-22%]">
-                        <img src={aboutCardImg} alt="" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+      <section>
+          <div className='container'>
+              <div className='flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row' >
+                  {/* ================= About img ================== */}
+                  <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
+                      <img src={aboutImg} alt="" />
+                      <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-7%] lg:right-[22%]">
+                          <img src={aboutCardImg} alt="" />
+                      </div>
+                  </div>
+
+                  {/* ================= About Content ================== */}
+                  <div className="w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2">
+                      <h2 className='heading'>Proud to be one of the nations best</h2>
+                      <p className='text__para'>
+                          For 30 years in a row, U.S. News and world reports has recognized us as one of the best publics
+                          hospitals in the Nation #1 in Sri Lanka.
+                      </p>
+
+                      <p className='text__para mt-[30px]'>
+                          Our best is something we strive for each day, caring for our patients-not looking at that we
+                          accomplished but towards what we can do tomorrow. Providing the best.
+                      </p>
+
+                      <Link to='/'>
+                          <button className='btn'>Learn More</button>
+                      </Link>
+                  </div>
+              </div>
+          </div>
+      </section>
+
   )
 }
 
