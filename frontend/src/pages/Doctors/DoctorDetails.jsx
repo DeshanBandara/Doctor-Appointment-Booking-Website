280323e9f0ebce49b6bcdae4f9497cb254ff5e25
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-17 17:45:21 +0530

    modified

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, {useState, userState} from 'react'
+import doctorImg from '../../assets/images/doctor-img02.png'
+import starIcon from '../../assets/images/Star.png'
 
 const DoctorDetails = () => {
-  return (
-    <div>DoctorDetails</div>
-  )
+
+  const [tab, setTab] = useState('about')
+
+  return <section>
+    <div className="max-w-[1170px] px-5 mx-auto">
+      <div className="grid md:grid-cols-3 gap-[50px]">
+        <div className="md:col-span-2">
+          <div className="flex items-center gap-5">
+            <figure className='max-w-[200px] max-h-[200px]'>
+              <img src={doctorImg} alt="" className='w-full mt-5 mb-5' />
+            </figure>
+
+            <div>
+              <span className='bg-[#CCF0F3] text-[#438fa4] py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4
+              lg:text-[16px] lg:leading-7 font-semibold rounded'>Surgeon</span>
+              <h3 className='text-black text-[22px] leading-9 mt-3 font-bold'>
+                Deshan Bandara
+              </h3>
+              <div className="flex items-center gap-[6px]">
+                <span className='flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 
+                font-semibold text-black'>
+                  <img src={starIcon} alt="" /> 4.8
+                </span>
+                <span className='text-[14px] leading-5 lg:text-[16px] lg:leading-7 
+                font-[400] text-textColor'>(272)</span>
+              </div>
+
+                <p className='text__para text-[12px] leading-6 md:text-[13px] lg:max-w-[390px]'>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo quos nemo doloremque placeat. 
+                  Id aliquam nobis quis expedita,
+                  </p>
+
+            </div>
+          </div>
+
+          <div className="mt-[50px] border-b border-solid border-[#0066ff34]">
+            <button 
+            onClick={() => setTab('about')}
+            className={` ${tab==='about' && 'border-b border-solid border-blue-500'} py-2 px-5 mr-5 text-[16px] leading-7 text-black font-semibold`}>
+              About
+            </button>
+            <button 
+            onClick={() => setTab('feedback')}
+              className={` ${tab === 'feedback' && 'border-b border-solid border-blue-500'} py-2 px-5 mr-5 text-[16px] leading-7 text-black font-semibold`}>
+              Feedback
+            </button>
+          </div>
+
+          <div className="mt-[50px]">
+            
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <br />
+  </section>
 }
 
 export default DoctorDetails
