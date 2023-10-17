2f669df8ce545680ac2d2e11f34393866b8347df
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-17 17:01:10 +0530

    added padding size

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -5,14 +5,13 @@ import Testimonial from '../../components/Testimonial/Testimonial'
 
 const Doctors = () => {
   return <>
-    <section className='bg-[#fff9ea]'>
+    <section className='bg-[#fff9ea] p-10'>
       <div className="container text-center">
         <h2 className='heading'>Find a Doctor</h2>
         <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between">
           <input type="search" className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor' placeholder='Search Doctor' />
           <button className='btn mt-0 rounded-[0px] rounded-r-md'>Search</button>
         </div>
-        <br /><br /><br />
       </div>
     </section>
 
