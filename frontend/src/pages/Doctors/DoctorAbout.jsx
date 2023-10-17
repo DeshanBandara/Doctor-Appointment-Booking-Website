ceb809d397f8d91d62fe0cb873a6ececee4208fd
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-17 18:26:54 +0530

    modified

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { formateDate } from '../../utils/formateDate'
 
 const DoctorAbout = () => {
   return (
-    <div>DoctorAbout</div>
+    <div>
+        <div>
+            <h3 className='text-[20px] leading-[30px] text-black font-semibold fles items-center gap-2'>About of 
+                  <span className='text-[#246BCE] font-bold text-[24px] leading-9 ml-3'>
+                     Deshan Bandara 
+                </span>
+            </h3>
+            <p className="text__para">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, consectetur alias. 
+                Consequuntur voluptatum recusandae sit,
+                hic animi maiores alias! Aliquid, sed rem quo explicabo autem quis eligendi magnam vero adipisci?
+            </p>
+        </div>
+
+        <div className='mt-12'>
+            <h3 className='text-[20px] leading-[30px] text-black font-semibold'>Education</h3>
+              <ul className='pt-4 md:p-5'>
+                  <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                      <div>
+                          <span className='text-[#246BCE] text-[15px] leading-6 font-semibold'>
+                              {formateDate("08-29-2010")} - {formateDate("08-31-2015")}
+                          </span>
+                          <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeon</p>
+                      </div>
+                      <p className='text-[14px] leading-5 font-medium text-textColor'>New Asiri Hospital, Colombo</p>
+                  </li>
+                  <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                      <div>
+                          <span className='text-[#246BCE] text-[15px] leading-6 font-semibold'>
+                              {formateDate("10-01-2015")} - {formateDate("10-04-2018")}
+                          </span>
+                          <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeon</p>
+                      </div>
+                      <p className='text-[14px] leading-5 font-medium text-textColor'>New Asiri Hospital, Colombo</p>
+                  </li>
+              </ul>
+        </div>
+
+        <div className="mt-12">
+              <h3 className='text-[20px] leading-[30px] text-black font-semibold'>Education</h3>
+        </div>
+
+        
+
+    </div>
   )
 }
 
