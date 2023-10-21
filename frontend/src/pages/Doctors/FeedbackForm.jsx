f81b856dc15ce5de4378332048f9dafc5ef784c3
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-21 23:02:52 +0530

    updated with feedback details

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -6,6 +6,11 @@ const FeedbackForm = () => {
 
 const [rating, setRating] = useState(0);
 const [hover, setHover] = useState(0);
+const [reviewText, setReviewText] = useState("")
+
+const handleSubmitReveiw = async e => {
+    e.preventDefault()
+}
 
   return (
     <form action="">
@@ -46,8 +51,14 @@ const [hover, setHover] = useState(0);
 
         <div className="mt-[30px]">
         <h3 className='text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0'>
-                How would you rate the overall experiance?
+                Share your feedback suggestions.
             </h3>
+
+            <textarea className="border border-solid border-[#0066ff34] focus:outline outline-[rgb(59,135,249)] w-full px-6 py-3 rounded-md"
+            placeholder="Write your message..."
+            onChange={e => setReviewText(e.target.value)} 
+            name="" id="" cols="30" rows="5"></textarea>
+            <button type="submit" className="btn" onClick={handleSubmitReveiw}>Submit Feedback</button>
         </div>
 
     </form>
