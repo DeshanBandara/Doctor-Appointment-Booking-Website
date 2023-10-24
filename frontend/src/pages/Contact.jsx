d5b4e0b5aa91d9b7e2abb28ead615eb04be4a2c8
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-24 17:47:05 +0530

    Updated

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+//import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section>
+      <div className="px-4 mx-auto max-w-screen-md">
+        <h2 className='heading text-center'>Contact Us</h2>
+        <p className='mb-8 lg:mb-16 font-light text-center text__para'>
+          Got a technical issue? Want to send feedback about a beta feature? let us know.
+        </p>
+        <form action="#" className='space-y-8'>
+          <div>
+            <label htmlFor="email" className='form__label'>
+              Your Email
+            </label>
+            <input 
+              type="email" 
+              id='email'
+              placeholder='example@gmail.com'
+              className='form__input mt-1'
+            />
+          </div>
+          <div>
+            <label htmlFor="submit" className='form__label'>
+              Subject
+            </label>
+            <input 
+              type="email" 
+              id='submit'
+              placeholder='Let us know how we can help you'
+              className='form__input mt-1'
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="Message" className='form__label'>
+              Your Message
+            </label>
+            <textarea
+              rows='6'
+              type="email" 
+              id='message'
+              placeholder='Leave a comment ....'
+              className='form__input mt-1'
+            />
+          </div>
+          <button type="submit" className="btn rounded sm:w-fit">Submit</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
