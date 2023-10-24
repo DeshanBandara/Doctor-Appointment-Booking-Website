e6606f3b1083a9ff31c6bc55dac05998c8bbb812
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-24 17:47:35 +0530

    Updated

--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 
 const SidePanel = () => {
   return (
-    <div>SidePanel</div>
+    <div className="shadow-panelShadow p-3 lg:p-5 rounded-md">
+      <div className="flex items-center justify-between">
+        <p className="text__para mt-0 font-semibold">Ticket Price</p>
+        <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold'>
+          LKR 500
+        </span>
+      </div>
+
+      <div className="mt-[30px]">
+        <p className='text__para mt-0 font-semibold text-headingColor'>
+          Available Time Slots:
+        </p>
+
+        <ul className='mt-3'>
+          <li className='flex items-center justify-between mb-2'>
+            <p className='text-[15px] leading-6 text-textColor font-semibold'>
+              Sunday
+            </p>
+            <p className='text-[15px] leading-6 text-textColor font-semibold'>
+              4:00 PM - 9.30PM
+            </p>
+          </li>
+
+          <li className='flex items-center justify-between mb-2'>
+            <p className='text-[15px] leading-6 text-textColor font-semibold'>
+              Monday
+            </p>
+            <p className='text-[15px] leading-6 text-textColor font-semibold'>
+              4:00 PM - 9.30PM
+            </p>
+          </li>
+
+          <li className='flex items-center justify-between mb-2'>
+            <p className='text-[15px] leading-6 text-textColor font-semibold'>
+              Wednsday
+            </p>
+            <p className='text-[15px] leading-6 text-textColor font-semibold'>
+              4:00 PM - 9.30PM
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <button className='btn px-2 w-full rounded-md'>Book Appoinment</button>
+
+    </div>
   )
 }
 
