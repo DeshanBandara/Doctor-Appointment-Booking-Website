4faaecaa7b037311a33af859990cdb65ab3acca7
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-11-02 19:39:39 +0530

    added nodemon

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,18 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8000
+
+const corsOptions = {
+    origin:true
+}
+
+app.get('/',(req,res)=>{
+    res.send('Api is Working')
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+})
