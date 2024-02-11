d95cc5d72b163b7629dc7904cf369e34c7facd80
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-02-11 10:57:15 +0530

    Modified

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import authRoute from "./Routes/auth.js"
 
 dotenv.config()
 
@@ -17,12 +18,29 @@ app.get('/',(req,res)=>{
     res.send('Api is Working') //will show the output in the browser
 })
 
+//database coonection
+mongoose.set('strictQuery', false)
+const connectDB = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        console.log("MongoDB database is connected")
+
+    } catch (err) {
+        console.log("MongoDB database is connection failed")
+    }
+}
 
 //Middlware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoute) //domain/api/v1/auth/register
 
 app.listen(port, () => {
-    console.log("Server is running on port " + port); //show the output in command line inerface 
+    connectDB();
+    console.log("Server is running on port " + port); //show the output in command line interface
 })
